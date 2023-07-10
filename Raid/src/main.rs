dd0974a6bcc2bb4d882ac24b7318ccd518247684
Artem <Palorel@yandex.ru>
2023-07-10 23:41:11 +0300

    Функция main

--- Raid/src/main.rs
+++ Raid/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+mod hamming;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut inputLineSizeDisks = String::new();
+    println!("Введите колличество дисков: ");
+    io::stdin().read_line(&mut inputLinesizeDisks);
+    let mut inputSizeDisks = inputLinesizeDisks.trim().parse().expect("Input not an integer");
+    let vectorDisks: Vec<Vec> = Vec::new();
+    let mut inputLineSizeDisk = String::new();
+    
+    for pointer in 0..inputSizeDisks {
+        println!("Введите колличество данных ");
+        io::stdin().read_line(&mut inputLinesizeDisk);
+        let mut inputSizeDisk = inputLineSizeDisk.trim().parse().expect("Input not an integer");
+
+    }
 }
