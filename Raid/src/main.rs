c7388a6f1f9837e68b66a0a474c0e3a795c36946
Artem <Palorel@yandex.ru>
2023-07-11 11:51:08 +0300

    Изменение под условие задачи

--- Raid/src/main.rs
+++ Raid/src/main.rs
@@ -5,15 +5,24 @@ mod hamming;
 fn main() {
     let mut inputLineSizeDisks = String::new();
     println!("Введите колличество дисков: ");
+    println!("Введите размер дисков: ")
     io::stdin().read_line(&mut inputLinesizeDisks);
     let mut inputSizeDisks = inputLinesizeDisks.trim().parse().expect("Input not an integer");
-    let vectorDisks: Vec<Vec> = Vec::new();
+    let arrayOfVectorsDisks: [Vec, inputSizeDisks];
     let mut inputLineSizeDisk = String::new();
-    
-    for pointer in 0..inputSizeDisks {
+    let mut inputLineNumber = String::new();
+
+    for pointerForArray in 0..inputSizeDisks {
         println!("Введите колличество данных ");
-        io::stdin().read_line(&mut inputLinesizeDisk);
+        io::stdin().read_line(&mut inputLineSizeDisk);
         let mut inputSizeDisk = inputLineSizeDisk.trim().parse().expect("Input not an integer");
 
+        for pointerForVector in 0..inputSizeDisk {
+            println!("Введите данные, числа");
+            io::stdin().read_line(&mut inputNumber);
+            let mut inputNumber = inputLineNumber.trim().parse().expect("Input not an integer");
+            arrayOfVectorsDisks[pointerForArray].push(inputNumber);
+        }
     }
+    hamming.addEccDisks(arrayOfVectorsDisks);
 }
