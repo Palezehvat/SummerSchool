bbdfe512c0a10fee1346ba09cd808ecd5a343d48
Artem <Palorel@yandex.ru>
2023-07-11 13:21:00 +0300

    теперь компилится

--- Raid/src/main.rs
+++ Raid/src/main.rs
@@ -1,28 +1,28 @@
 use std::io;
-mod hamming;
+//mod hamming;
 
 
 fn main() {
-    let mut inputLineSizeDisks = String::new();
+    let mut input_line_size_disks = String::new();
+    let mut input_line_size_disk = String::new();
     println!("Введите колличество дисков: ");
-    println!("Введите размер дисков: ")
-    io::stdin().read_line(&mut inputLinesizeDisks);
-    let mut inputSizeDisks = inputLinesizeDisks.trim().parse().expect("Input not an integer");
-    let arrayOfVectorsDisks: [Vec, inputSizeDisks];
-    let mut inputLineSizeDisk = String::new();
-    let mut inputLineNumber = String::new();
+    io::stdin().read_line(&mut input_line_size_disks);
+    println!("Введите размер дисков: ");
+    io::stdin().read_line(&mut input_line_size_disk);
+    let mut input_size_disks = input_line_size_disks.trim().parse().expect("Input not an integer");
+    let mut input_size_disk = input_line_size_disk.trim().parse().expect("Input not an integer");
+    
+    let mut matrix_disks = vec![vec![0; input_size_disk]; input_size_disks];
+    
+    let mut input_line_number = String::new();
 
-    for pointerForArray in 0..inputSizeDisks {
-        println!("Введите колличество данных ");
-        io::stdin().read_line(&mut inputLineSizeDisk);
-        let mut inputSizeDisk = inputLineSizeDisk.trim().parse().expect("Input not an integer");
-
-        for pointerForVector in 0..inputSizeDisk {
-            println!("Введите данные, числа");
-            io::stdin().read_line(&mut inputNumber);
-            let mut inputNumber = inputLineNumber.trim().parse().expect("Input not an integer");
-            arrayOfVectorsDisks[pointerForArray].push(inputNumber);
+    for pointer_for_array in 0..input_size_disks {
+        println!("Введите данные, числа: ");
+        for pointer_for_vector in 0..input_size_disk {
+            io::stdin().read_line(&mut input_line_number);
+            let mut input_number = input_line_number.trim().parse().expect("Input not an integer");
+            matrix_disks[pointer_for_array][pointer_for_vector] = input_number;
         }
     }
-    hamming.addEccDisks(arrayOfVectorsDisks);
+    //hamming.addEccDisks(matrixDisks);
 }
