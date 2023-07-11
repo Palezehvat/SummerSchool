012a8f7aedc4a50787f4e06b89034c8b5bcca949
Artem <Palorel@yandex.ru>
2023-07-11 20:11:11 +0300

    Корректировки

--- Raid/src/main.rs
+++ Raid/src/main.rs
@@ -12,20 +12,16 @@ fn main() {
     let mut input_size_disks = input_line_size_disks.trim().parse().expect("Input not an integer");
     let mut input_size_disk = input_line_size_disk.trim().parse().expect("Input not an integer");
     
-    let mut matrix_disks = vec![vec!['0'; input_size_disk]; input_size_disks];
+    let mut matrix_disks = vec![vec![0; input_size_disk]; input_size_disks];
     
-    let mut input_line_char = String::new();
+    let mut input_line_int = String::new();
 
     for pointer_for_array in 0..input_size_disks {
         println!("Введите данные, числа: ");
         for pointer_for_vector in 0..input_size_disk {
-            io::stdin().read_line(&mut input_line_char);
-            let char_vec: Vec<char> = input_line_char.chars().collect();
-            if char_vec.len() > 1 {
-                std::panic!();
-            }
-            matrix_disks[pointer_for_array][pointer_for_vector] = char_vec[0];
+            io::stdin().read_line(&mut input_line_int);
+            matrix_disks[pointer_for_array][pointer_for_vector] = input_line_int.trim().parse().expect("Input not an integer");
         }
     }
-    let mut matrix_ecc = hamming.addEccDisks(matrix_disks);
+    let mut matrix_ecc = hamming::add_ecc_disks(input_size_disks, input_size_disk, matrix_disks);
 }
